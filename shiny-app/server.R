ab345d72c7f9062d5bbc70ad52b71edc351cfb84
BobbyCurrent <feliciacurrent@dhcp-10-250-39-98.harvard.edu>
2020-10-22 17:17:40 -0400

    Milestone 5 Done, need to get help with data cleaning

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -12,16 +12,16 @@ school_spending_2017_2018 <-
     spending_data_2018 <-
     read_excel("data/Stfis180_1a.xlsx") %>%
     group_by(STNAME) %>%
-    select(STNAME, R1A)
+    select(STNAME, E11A)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$carPlot <- renderPlot({
         school_spending_2017_2018 %>%
-            ggplot(aes(x = STNAME, y = R1A)) +
+            ggplot(aes(x = STNAME, y = E11A)) +
             geom_col() +
-            labs(x = "State Name", y = "Some Kind of Spending") +
+            labs(x = "State", y = "Total Amount Paid", title = "The Total Amount Paid to Teachers in Regular Education Programs by State", subtitle = "2017 - 2018 School Year") +
             theme(axis.text.x = element_text(angle = 90, hjust = 1))
     
        
